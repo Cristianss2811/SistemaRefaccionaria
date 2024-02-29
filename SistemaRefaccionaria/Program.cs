6e82e54b263a7a2ab04c4bfd733c84ab745a413b
Cristianss2811 <cristianss.2811@gmail.com>
2024-02-29 10:28:22 -0600

    Patron de Repositorio

--- SistemaRefaccionaria/Program.cs
+++ SistemaRefaccionaria/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SistemaRefaccionaria.AccesoDatos.Data;
+using SistemaRefaccionaria.AccesoDatos.Repositorio.IRepositorio;
+using SistemaRefaccionaria.AccesoDatos.Repositorio;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddScoped<IUnidadTrabajo, UnidadTrabajo>();
 
 var app = builder.Build();
 
