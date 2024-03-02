5badd422e0cdae91c4eb8349b032e2ce1c2572ed
Cristianss2811 <cristianss.2811@gmail.com>
2024-03-02 15:08:37 -0600

    Mostrar TablaBodegas Espanol

---
+++ SistemaRefaccionaria/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaRefaccionaria.AccesoDatos.Repositorio.IRepositorio;
+
+namespace SistemaRefaccionaria.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public BodegaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Bodega.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+        #endregion
+    }
+}
